bc6c61e7c0837973b4a3baddfe1d6667d37474b6
sbolaris <stephen_bolaris@bio-rad.com>
2022-03-01 21:47:25 +0000

    added spiketype as well to be captured correctly even if user is using AWS batch

--- src/generateRmdReport.R
+++ src/generateRmdReport.R
@@ -15,6 +15,8 @@ comArgs <- commandArgs(TRUE)
 base_dir <- comArgs[1]
 temp_dir <- comArgs[2]
 anno_dir <- comArgs[3]
+genomeid <- comArgs[4]
+spikedin <- comArgs[5]
 
 names = system("ls ./out/counts/",intern=T ) 
 names = unlist(lapply(names, function(x) unlist(strsplit(x, "\\."))[3]))
@@ -191,12 +193,14 @@ for(n in names){
 	containerInfo <- data.frame("rowname" = containerInfo[,1], env = containerInfo[,2], stringsAsFactors=FALSE)
 	containerInfo[3,2] <- substr(containerInfo[3,2], 1,8)
 	anno_path <- unlist(strsplit(anno_dir,"/"))
-	referenceGenome <- anno_path[grepl("hg38|mm10|rnor6|tair10|sacCer3|dm6|danRer11|ce11", anno_path)]
+	#referenceGenome <- anno_path[grepl("hg38|mm10|rnor6|tair10|sacCer3|dm6|danRer11|ce11", anno_path)]
+	referenceGenome <- genomeid
 	if(length(referenceGenome) == 0)
 	{
 		        referenceGenome = "NA"
 	}
-	isErcc <- any(grepl("ercc", anno_path))
+	#isErcc <- any(grepl("ercc", anno_path))
+	isErcc <- spikedin
 	write(paste("Preparing to read: ", paste(anno_dir,"annotation_version.txt", sep="/")), stderr())
 	anno_version <- read.table(paste(anno_dir,"annotation_version.txt", sep="/"), comment.char="", fill=T, sep=",")
 	write("Read annotation_version", stderr())
